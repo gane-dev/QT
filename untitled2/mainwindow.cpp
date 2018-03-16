d498d7acf53d90a492b48c4440ba4c6c0b53d864
ganesh <gadeva.mba@gmail.com>
2018-03-15 23:45:58 -0400

    init version

---
+++ untitled2/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QDebug>
+#include<QInputDialog>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    mTasks()
+{
+    ui->setupUi(this);
+    connect(ui->addTaskButton,&QPushButton::clicked,this,&MainWindow::addTask);
+}
+void MainWindow::addTask(){
+    qDebug() << "User clicked the button";
+    bool ok;
+        QString name = QInputDialog::getText(this,
+            tr("Add task"),
+            tr("Task name"),
+            QLineEdit::Normal,
+            tr("Untitled task"),               &ok);
+        if (ok && !name.isEmpty()) {
+            qDebug() << "Adding new task";
+            Task* task = new Task(name);
+            mTasks.append(task);
+            ui->tasksLayout->addWidget(task);
+        }
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
